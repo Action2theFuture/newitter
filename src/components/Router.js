85547bdb28d345967853de23d205b74f2be7a2d9
Action2theFuture <dnstks0204@naver.com>
2020-12-29 22:44:25 +0900

    LogOut Commit

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile"
+import Navigation from "components/Navigation"
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                     <>
                         <Route exact path="/">
                             <Home />
                         </Route>
-                    </> 
+                        <Route exact path="/Profile">
+                            <Profile />
+                        </Route>
+                        <Redirect from="*" to="/" />
+                    </>
                 ) : (
-                    <Route exact path="/">
-                        <Auth />
-                    </Route>
-                )}
+                        <>
+                            <Route exact path="/">
+                                <Auth />
+                            </Route>
+                            <Redirect from="*" to="/" />
+                        </>
+                    )}
             </Switch>
         </Router>
     );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,16 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-export default () => <span>Profile</span>
+export default () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    };
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
